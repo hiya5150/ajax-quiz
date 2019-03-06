ea16f0ae74a748905a78fe97fcab2bf57f02136f
unknown <5828537@gmail.com>
2019-03-06 11:31:47 -0500

    firstVersion

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 //Run jQuery after the document is fully loaded.
-$(document).ready(
+$(document).ready(function() {
     //The function that does the stuff.
-    function () {
-        //Make the AJAX call
-        $.ajax('http://api.tvmaze.com/singlesearch/shows?q=the+magicians&embed=episodes', {
+
+    $("btn1").click(function (e) {
+        e.preventDefault();
+        let currentShow = document.getElementById("name1").value;
+        console.log(currentShow);
+        'http://api.tvmaze.com/search/shows?q=:query';
+        $.ajax(currentShow, {
             method: "GET",
             dataType: "json"
         })
@@ -11,16 +15,21 @@ $(document).ready(
             .done(
                 function (data) {
                     //Add the name
-                    $('#name').append(data.name);
+                    // $('#name').append(data.name);
                     //Add the episodes
                     data._embedded.episodes.forEach(function (episode) {
-                        $('#episodeList').append('<tr>'+
+                        $('#episodeList').append('<tr>' +
                             '<td>' + episode.season + '</td>' +
                             '<td>' + episode.number + '</td>' +
                             '<td>' + episode.name + '</td>' +
                             '<td>' + episode.summary + '</td>' +
                             +' </tr>')
-            })
-        })
-    }
-)
+                    })
+                })
+    })
+
+    //Make the AJAX call
+
+
+
+})
